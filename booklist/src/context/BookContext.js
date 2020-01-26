78d3341563a26bfc5b770cb2ef026073af659d03
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-01-26 17:28:32 +0530

    context and state with useReduce

--- booklist/src/context/BookContext.js
+++ booklist/src/context/BookContext.js
@@ -1,25 +1,14 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
 import uuid from "uuid/v1";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, setBooks] = useState([
-    { title: "Sapiens", author: "Yuval Noah Harari", id: 1 },
-    { title: "Angels and Demons", author: "Dan Brown", id: 2 },
-    { title: "Blink", author: "Malcolm Gladwell", id: 3 }
-  ]);
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: uuid() }]);
-  };
-
-  const removeBook = id => {
-    setBooks(books.filter(book => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
-    <BookContext.Provider value={{books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
